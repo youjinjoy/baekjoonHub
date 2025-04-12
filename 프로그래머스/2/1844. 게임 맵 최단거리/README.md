008d7992b1feb42bf6f80ea57e8e684b3d906012
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-04-12 22:49:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 17.98 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 38.4 MB, 시간: 41.54 ms
+메모리: 38.8 MB, 시간: 17.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 19일 21:26:54
+2025년 04월 12일 22:49:04
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,32 +1,34 @@
 function solution(maps) {
-    let answer = -1;
-    
     const n = maps.length;
     const m = maps[0].length;
     
     const visited = Array.from({length: n}, () => Array(m).fill(false));
-          
-    const dx = [-1, 1, 0, 0];
-    const dy = [0, 0, -1, 1];
-    
-    const queue = [[0,0,1]];
     visited[0][0] = true;
-    while (queue.length) {
-        const [cx, cy, d] = queue.shift();
-        
+    
+    const queue = [[0,0,1]];    
+    let head = 0;
+    
+    const dx = [1, -1, 0, 0];
+    const dy = [0, 0, 1, -1];
+
+    while (head < queue.length) {
+
+        const [cx, cy, step] = queue[head++];
+       
         for (let i = 0 ; i < 4 ; i++) {
             const [nx, ny] = [cx+dx[i], cy+dy[i]];
-            
-            if (nx === n-1 && ny === m-1) {
-                return d + 1;
+             if (nx === n - 1 && ny === m - 1) {
+                return step + 1;
             }
-            
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] && !visited[nx][ny]) {
+
+            const isInside = 0 <= nx && nx < n && 0 <= ny && ny < m;
+
+            if (isInside && maps[nx][ny] === 1 && !visited[nx][ny]) {
                 visited[nx][ny] = true;
-                queue.push([nx,ny, d+1]);
+                queue.push([nx, ny, step + 1]);
             }
         }
     }
     
-    return answer;
+    return -1;
 }
