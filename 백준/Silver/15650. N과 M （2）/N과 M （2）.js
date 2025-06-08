4ccf417cdddcd207636c9e593a20163915f6b1db
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-06-08 18:33:49 +0900

    [Silver III] Title: N과 M (2), Time: 96 ms, Memory: 9456 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.js
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -8,8 +8,6 @@ const input = fs
 
 const [N, M] = input[0].split(" ").map((item) => parseInt(item));
 
-const visited = Array(M + 1).fill(false);
-
 function backtrack(current, start) {
   if (current.length === M) {
     console.log(current.join(" "));
@@ -17,13 +15,9 @@ function backtrack(current, start) {
   }
 
   for (let i = start; i <= N; i++) {
-    if (!visited[i]) {
-      visited[i] = true;
-      current.push(i);
-      backtrack(current, i + 1);
-      current.pop();
-      visited[i] = false;
-    }
+    current.push(i);
+    backtrack(current, i + 1);
+    current.pop();
   }
 }
 
