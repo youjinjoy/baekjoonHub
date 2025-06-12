1679494a9ad0e762888b64fd2f9ae070259bc556
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-06-12 23:59:32 +0900

    [Silver II] Title: N과 M (12), Time: 148 ms, Memory: 10440 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = fs
+  .readFileSync(path, "utf8")
+  .split("\n")
+  .map((item) => item.trim());
+
+const [N, M] = input[0].split(" ").map((item) => parseInt(item));
+
+const numbers = input[1].split(" ").map((item) => parseInt(item));
+numbers.sort((a, b) => a - b);
+
+const answer = [];
+const visited = Array(N).fill(false);
+
+function backtrack(current, start) {
+  if (current.length === M) {
+    answer.push(current.join(" "));
+    return;
+  }
+
+  let prev = -1;
+  for (let i = start; i < N; i++) {
+    if (prev !== numbers[i]) {
+      current.push(numbers[i]);
+      prev = numbers[i];
+      backtrack(current, i);
+      current.pop();
+    }
+  }
+}
+
+backtrack([], 0);
+console.log(answer.join("\n"));
