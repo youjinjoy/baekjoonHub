b8bd451e8a77c490cc2a5def51e6cb0c9ec68692
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-10-05 20:01:38 +0900

    [Silver III] Title: 소수 구하기, Time: 344 ms, Memory: 42788 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,31 +1,19 @@
 import sys
-from typing import List
-import heapq as hq
-from collections import deque
 import math
 
 input = sys.stdin.readline
 
 a, b = list(map(int, input().rstrip().split(' ')))
 
-for n in range(a, b + 1):
-    
-    if n == 1:
-        continue
-    
-    if n == 2:
-        print(n)
-        continue
-    
-    if n % 2 == 0:
-        continue
-    
-    m = math.isqrt(n)
-    is_prime_number = True
-    for i in range(3, m + 1, 2):
-        if n % i == 0:
-            is_prime_number = False
-            break
+prime_number = [True for _ in range(b + 1)]
+prime_number[0] = prime_number[1] = False
 
-    if is_prime_number:
-        print(n)
+for i in range(2, math.isqrt(b) + 1):
+
+    if prime_number[i]:
+        for j in range(i*i, b + 1, i):
+            prime_number[j] = False
+
+for k in range(a, b+1):
+    if prime_number[k]:
+        print(k)
