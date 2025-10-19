4dd673badda1e9b4fc3845fb3a2db46d92a4e07b
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-10-19 22:20:41 +0900

    [Silver II] Title: 좌표 압축, Time: 1948 ms, Memory: 151316 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,4 +1,5 @@
 import sys
+from bisect import bisect_left
 input = sys.stdin.readline
 
 N = int(input())
@@ -6,20 +7,5 @@ A = list(map(int, input().split(' ')))
 
 SA = sorted(set(A))
 
-def lower_bound(start, end, arr, target):
-    while start <= end:
-        mid = (start + end) // 2
-        if arr[mid] < target:
-            start = mid + 1
-        else:
-            end = mid - 1
-    return start
-
-cache = {}
-start = 0
-end = len(SA) - 1
-for a in SA:
-    cache[a] = lower_bound(start, end, SA, a)
-
 for a in A:
-    print(cache[a])
+    print(bisect_left(SA, a))
