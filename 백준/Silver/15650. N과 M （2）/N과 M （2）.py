34c33f1ffebec8cfacd3302a878f79b0dae0ccee
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-11-22 15:58:43 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,15 +1,24 @@
 import sys
-from collections import deque
+
 input = sys.stdin.readline
 
-N,M = map(int,input().split(' '))
+N, M = map(int, input().split(' '))
+visited = [False for _ in range(N + 1)]
+result = []
 
-def dfs(depth,start,current):
-  if depth == M:
-    print(' '.join(map(str,current)))
-    return
+def dfs(k):
+    if len(result) == M:
+        print(*result)
+        return
 
-  for i in range(start,N+1):
-    dfs(depth+1,i+1,current+[i])
+    for i in range(k, N + 1):
+        if not visited[i]:
+            visited[i] = True
+            result.append(i)
 
-dfs(0,1,[])
+            dfs(i + 1)
+
+            visited[i] = False
+            result.pop()
+
+dfs(1)
