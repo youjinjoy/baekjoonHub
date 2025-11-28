29cabc0b10e7c709d7ba9fc7f654fd7f1776f579
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-11-28 22:35:02 +0900

    [level 2] Title: 올바른 괄호, Time: 6.15 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,23 +1,18 @@
 def solution(s):
-    answer = True
-    # 스택 2개 준비
-    # 하나는 ) 저장
-    # ) 저장된 스택이 비어있지만, 다른 스택에서 (가 꺼내지면 Fail
-    
-    s1 = list(s)
-    s2 = []
-    
-    while s1:
-        c = s1.pop()
 
-        if c == ')':
-            s2.append(c)
-        else: # c == '('
-            if len(s2) == 0:
-                return False
-            s2.pop()
+    answer = True
     
-    if len(s2) != 0:
+    stack = []
+    
+    for c in s:
+        if c =='(':
+            stack.append('(')
+        else:
+            if not stack:
+                return False
+            stack.pop()
+    
+    if stack:
         return False
-        
+    
     return True
