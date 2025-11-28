bd1330fd239d88a9856e3ab3e0b9db85bb2dbcde
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-11-29 01:08:42 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 65.26 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 9.14 MB, 시간: 65.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 30일 16:27:44
+2025년 11월 29일 01:08:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,20 +1,24 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    w = 0
-    time = 0
-    q = deque([0 for _ in range(bridge_length)])
-    for truck in truck_weights:
-        while True:
-            w -= q.popleft()
-            time+=1
-            if w+truck > weight:
-                q.append(0)
-            else:
-                q.append(truck)
-                w += truck
-                break
-
-    time += bridge_length
     
-    return time
+    trucks = deque(truck_weights)
+    bridges = deque([0 for _ in range(bridge_length)])
+    t = 0
+    w = 0
+
+    while trucks or w != 0:
+        t += 1
+
+        popped = bridges.popleft()
+        w -= popped
+        
+        if trucks and w + trucks[0] <= weight:
+            cw = trucks.popleft()
+            w += cw
+            bridges.append(cw)
+        else:
+            bridges.append(0)
+        
+    
+    return t
