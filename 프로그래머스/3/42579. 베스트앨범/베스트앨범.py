b125d148adfeed78da41e62f99589b04d678973f
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-11-28 16:39:13 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -2,37 +2,21 @@ from collections import defaultdict
 
 def solution(genres, plays):
     
-    # 총 재생 수 내림차순으로 정렬된 장르
-    S = set(genres)
-    genre_order = list(S)
-    
-    
-    # 장르 키 - (재생 수 내림차순, 고유 번호 오름차순)으로 정렬된 고유 번호 배열
+    # 장르 키 - (재생 수 내림차순, 고유 번호 오름차순)으로 정렬된 고유 번호 / 개별 재생 수 배열
     G = defaultdict(list)
-    
-    
-    #
-    N = len(genres)
+    # 장르 키 - 총 재생 수
     total_plays = defaultdict(int)
-    for i in range(N):
-        G[genres[i]].append(i)
-        total_plays[genres[i]] += plays[i]
-    
-    #
-    genre_order.sort(key = lambda g: total_plays[g], reverse = True)
 
-    #
-    for g in genre_order:
-        G[g].sort(key = lambda i: (-plays[i], i))
-    
-    #
+    for i in range(len(genres)):
+        G[genres[i]].append((i, plays[i]))
+        total_plays[genres[i]] += plays[i]
+
+    # 총 재생 수 내림차순으로 정렬된 장르
+    genre_order = sorted(total_plays.keys(), key = lambda g: total_plays[g], reverse = True)
+
     answer = []
     for g in genre_order:
-        cnt = 0
-        for music_num in G[g]:
-            answer.append(music_num)
-            cnt += 1
-            if cnt >= 2:
-                break
-    
+        sorted_g = sorted(G[g], key = lambda x: (-x[1], x[0]))
+        answer.extend(num for num, _ in sorted_g[:2])
+                            
     return answer
