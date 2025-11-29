1bc18fdf86a18ff8b38e458782765c6a9580737c
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-11-29 16:40:02 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.08 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,24 +1,38 @@
 import heapq as hq
 
 def solution(jobs):
-    heap = []
-    count = 0
-    answer = 0
-    b= -1
+
     
-    jobs.sort()
-    time = jobs[0][0]
-    b = time-1
-    while count < len(jobs):
-        for s,t in jobs:
-            if b < s <= time:
-                hq.heappush(heap, [t,s])
-        if heap:
-            count += 1
-            [t,s] = hq.heappop(heap)
-            b = time
-            time += t
-            answer += time - s
-        else:
-            time += 1
-    return answer//len(jobs)
+    waiting_list = []
+    ready_list = []
+    
+    for i, job in enumerate(jobs):
+        start, duration = job
+        waiting_list.append((start, duration, i))
+    
+    waiting_list.sort(key = lambda x: x[0])
+
+    result = []
+    t = 0
+    idx = 0
+    
+    while waiting_list or ready_list:
+        
+        cnt = 0            
+        for s, d, i in waiting_list:
+            if s <= t:
+                hq.heappush(ready_list, (d, s, i))
+                cnt += 1
+
+        if cnt > 0:
+            waiting_list = waiting_list[cnt:]
+        
+        
+        if not ready_list:
+            t = waiting_list[0][0]
+        else:        
+            d, s, i = hq.heappop(ready_list)
+            t += d
+            result.append(t-s)
+    
+    return sum(result) // len(result)
