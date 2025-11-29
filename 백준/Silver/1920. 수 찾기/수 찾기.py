322f241142345ef043c5a02e0396981d5a7ae47a
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-11-30 00:39:40 +0900

    [Silver IV] Title: 수 찾기, Time: 212 ms, Memory: 51204 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,21 +1,17 @@
 import sys
-n=int(sys.stdin.readline())
-n_list=list(map(int,sys.stdin.readline().strip().split(' ')))
-n_len=len(n_list)
-m=int(sys.stdin.readline())
-m_list=list(map(int,sys.stdin.readline().strip().split(' ')))
+import bisect
+input = sys.stdin.readline
 
-# m_list 요소 하나하나가 n_list 안에 있는지 탐색 (이분 탐색)
-def binary_search(arr,target,start,end):
-   while start<=end:
-      mid=(start+end)//2
+N = int(input())
+numbers = list(map(int, input().split(' ')))
 
-      if arr[mid]==target: return 1
-      elif arr[mid]>target: end=mid-1
-      else: start=mid+1
-   return 0
+M = int(input())
+targets = list(map(int, input().split(' ')))
 
-n_list.sort()
-for ml in m_list:
-   result=binary_search(n_list,ml,0,n_len-1)
-   print(result)
+numbers.sort()
+for target in targets:
+    idx = bisect.bisect_left(numbers, target)
+    if idx < N and numbers[idx] == target:
+        print(1)
+    else:
+        print(0)
