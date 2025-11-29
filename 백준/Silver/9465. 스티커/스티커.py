92619a5d772cfe3f2e34cb96ba3071e4a4375920
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-11-30 00:16:04 +0900

    [Silver I] Title: 스티커, Time: 644 ms, Memory: 51480 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,33 +1,23 @@
 import sys
-from typing import List
+import heapq as hq
 
-def input():
-    return sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
-def read_int():
-    return int(input())
+T = int(input())
 
-def read_list() -> List[int]:
-    return list(map(int, input().split()))
+for _ in range(T):
+    N = int(input()) # 1 <= N <= 100,000
 
-def solve():
-    tc = read_int()
-    for _ in range(tc):
-        c = read_int()  # c >= 1
-        sticker = [read_list() for _ in range(2)]
+    arr = [list(map(int, input().split(' '))) for _ in range(2)]
 
-        # 0: 위 뜯음. 1: 아래 뜯음
-        dp = [[0 for _ in range(c)] for _ in range(2)]  # k 번째 열까지 최댓값
-        dp[0][0] = sticker[0][0]
-        dp[1][0] = sticker[1][0]
+    dp = [[0] * N for _ in range(2)]
+    dp[0][0], dp[1][0] = arr[0][0], arr[1][0]
 
-        if c >= 2:
-            dp[0][1] = sticker[1][0] + sticker[0][1]
-            dp[1][1] = sticker[0][0] + sticker[1][1]
-        for i in range(2, c):
-            dp[0][i] = max(dp[0][i-2] + sticker[0][i], dp[1][i-2] + sticker[0][i], dp[1][i-1] + sticker[0][i])
-            dp[1][i] = max(dp[1][i-2] + sticker[1][i], dp[0][i-2] + sticker[1][i], dp[0][i-1] + sticker[1][i])
-        
-        print(max(dp[0][-1],dp[1][-1]))
-        
-solve()
+    if N > 1:
+        dp[0][1], dp[1][1] = arr[0][1] + arr[1][0], arr[1][1] + arr[0][0]
+    
+    for i in range(2, N):
+        dp[0][i] = arr[0][i] + max(dp[1][i-1], dp[1][i-2])
+        dp[1][i] = arr[1][i] + max(dp[0][i-1], dp[0][i-2])
+    
+    print(max(dp[0][N-1], dp[1][N-1]))
