871252b212dafb109167815a8198bc1a3f613926
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-11-30 17:29:34 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 52 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,15 +1,20 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-N = int(input())
+tc = int(input())
 
-a = [0 for _ in range(11)] # n은 11보다 작은 양수
-a[1] = 1
-a[2] = 2
-a[3] = 4
-for i in range(4,11):
-    a[i] = a[i-1] + a[i-2] + a[i-3]
+M = 0
 
-for _ in range(N):
-    n = int(input())
-    print(a[n])
+numbers = [int(input()) for _ in range(tc)]
+dp = [0 for _ in range(12)]
+dp[1] = 1 # 1... 1
+dp[2] = 2 # 1+1, 2 ... 2
+dp[3] = 4 # 1+1+1, 1+2, 2+1, 3 ... 4
+# 1+1+1+1, 1+1+2, 1+2+1, 2+1+1, 1+3, 3+1 ... 7
+
+for i in range(4, 12):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+for n in numbers:
+    print(dp[n])
