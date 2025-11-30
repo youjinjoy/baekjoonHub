366aaadec43726608e41a6faca71b5fcda5b7761
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-11-30 11:35:50 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,5 +1,6 @@
 def solution(array, commands):
     answer = []
-    for [i,j,k] in commands:
-        answer.append(sorted(array[i-1:j])[k-1])
+    for command in commands:
+        i, j, k = command
+        answer.append(sorted(array[i - 1: j])[k - 1])
     return answer
