440f4bd4c94e1a3f874b93b38135efe51a4e56f1
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-11-30 15:28:17 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,35 +1,26 @@
-from collections import deque
 import sys
-input=sys.stdin.readline
+from collections import deque
+input = sys.stdin.readline
 
-n=int(input())
-m=int(input())
-graph=[[] for _ in range(n+1)]
-for i in range(m):
-    a,b=map(int,input().split(' '))
+N = int(input())
+M = int(input())
+
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = list(map(int, input().split(' ')))
     graph[a].append(b)
     graph[b].append(a)
 
-# print(graph)
 
-visited=[0]*(n+1)
-
-def bfs(v):
-    global answer
-    queue=deque()    
-    queue.append(v)
-
-    visited[v]=True
-    answer=0
+def dfs(cur, visited):
+    visited[cur] = True
     
-    while queue:
-        v=queue.popleft()
+    for nxt in graph[cur]:
+        if not visited[nxt]:
+            dfs(nxt, visited)
     
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i]=True
-                answer+=1
-    print(answer)
 
-bfs(1)
+visited = [False] * (N + 1)
+dfs(1, visited)
+print(sum(visited) - 1)
